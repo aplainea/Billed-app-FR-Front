5646e8e3e943ae73d1860fe2898f192e0d993e6d
Antoine P <antoine.plaineau@etudiant.univ-lr.fr>
2023-05-25 01:17:40 +0200

    fix(file): fix file and filename when is null

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -1,76 +1,103 @@
-import { ROUTES_PATH } from '../constants/routes.js'
-import Logout from "./Logout.js"
+import { ROUTES_PATH } from "../constants/routes.js";
+import Logout from "./Logout.js";
 
 export default class NewBill {
-  constructor({ document, onNavigate, store, localStorage }) {
-    this.document = document
-    this.onNavigate = onNavigate
-    this.store = store
-    const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
-    formNewBill.addEventListener("submit", this.handleSubmit)
-    const file = this.document.querySelector(`input[data-testid="file"]`)
-    file.addEventListener("change", this.handleChangeFile)
-    this.fileUrl = null
-    this.fileName = null
-    this.billId = null
-    new Logout({ document, localStorage, onNavigate })
-  }
-  handleChangeFile = e => {
-    e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    constructor({ document, onNavigate, store, localStorage }) {
+        this.document = document;
+        this.onNavigate = onNavigate;
+        this.store = store;
+        const formNewBill = this.document.querySelector(
+            `form[data-testid="form-new-bill"]`
+        );
+        formNewBill.addEventListener("submit", this.handleSubmit);
+        const file = this.document.querySelector(`input[data-testid="file"]`);
+        file.addEventListener("change", this.handleChangeFile);
+        this.fileUrl = null;
+        this.fileName = null;
+        this.billId = null;
+        new Logout({ document, localStorage, onNavigate });
+    }
+    handleChangeFile = (e) => {
+        this.document.getElementById("alert-message").hidden = true;
+        e.preventDefault();
+        let file = this.document.querySelector(`input[data-testid="file"]`)
+            .files[0];
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
+        /* Checks if the file extension is among the list of allowed MIME types */
+        const allowedMimeTypes = ["image/jpeg", "image/png"];
+        const isMimeTypeAllowed = allowedMimeTypes.includes(file.type);
+        if (isMimeTypeAllowed) {
+            const filePath = e.target.value.split(/\\/g);
+            const fileName = filePath[filePath.length - 1];
+            console.log("fileName", file);
+            const formData = new FormData();
+            const email = JSON.parse(localStorage.getItem("user")).email;
+            formData.append("file", file);
+            formData.append("email", email);
+
+            this.store
+                .bills()
+                .create({
+                    data: formData,
+                    headers: {
+                        noContentType: true,
+                    },
+                })
+                .then(({ fileUrl, key }) => {
+                    this.billId = key;
+                    this.fileUrl = fileUrl;
+                    this.fileName = fileName;
+                })
+                .catch((error) => console.error(error));
+        } else {
+            this.document.getElementById("alert-message").hidden = false;
+            e.target.value = ""; // reset the input
         }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
-  }
-  handleSubmit = e => {
-    e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
-    const email = JSON.parse(localStorage.getItem("user")).email
-    const bill = {
-      email,
-      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: 'pending'
-    }
-    this.updateBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
-  }
+    };
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(
+            'e.target.querySelector(`input[data-testid="datepicker"]`).value',
+            e.target.querySelector(`input[data-testid="datepicker"]`).value
+        );
+        const email = JSON.parse(localStorage.getItem("user")).email;
+        const bill = {
+            email,
+            type: e.target.querySelector(`select[data-testid="expense-type"]`)
+                .value,
+            name: e.target.querySelector(`input[data-testid="expense-name"]`)
+                .value,
+            amount: parseInt(
+                e.target.querySelector(`input[data-testid="amount"]`).value
+            ),
+            date: e.target.querySelector(`input[data-testid="datepicker"]`)
+                .value,
+            vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+            pct:
+                parseInt(
+                    e.target.querySelector(`input[data-testid="pct"]`).value
+                ) || 20,
+            commentary: e.target.querySelector(
+                `textarea[data-testid="commentary"]`
+            ).value,
+            fileUrl: this.fileUrl,
+            fileName: this.fileName,
+            status: "pending",
+        };
+        this.updateBill(bill);
+        this.onNavigate(ROUTES_PATH["Bills"]);
+    };
 
-  // not need to cover this function by tests
-  updateBill = (bill) => {
-    if (this.store) {
-      this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
-    }
-  }
+    // not need to cover this function by tests
+    updateBill = (bill) => {
+        if (this.store) {
+            this.store
+                .bills()
+                .update({ data: JSON.stringify(bill), selector: this.billId })
+                .then(() => {
+                    this.onNavigate(ROUTES_PATH["Bills"]);
+                })
+                .catch((error) => console.error(error));
+        }
+    };
 }
--- src/views/NewBillUI.js
+++ src/views/NewBillUI.js
@@ -1,8 +1,7 @@
-import VerticalLayout from './VerticalLayout.js'
+import VerticalLayout from "./VerticalLayout.js";
 
 export default () => {
-
-  return (`
+    return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -55,7 +54,8 @@ export default () => {
                   </div>
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
-                    <input required type="file" class="form-control blue-border" data-testid="file" />
+                    <input type="file" accept="image/jpeg, image/jpg, image/png" class="form-control blue-border" data-testid="file" required />
+                    <div id="alert-message" class="alert alert-danger mt-2" role="alert" hidden>Erreur format. Format autorisé: JPEG/JPG ou PNG</div>
                   </div>
                 </div>
             </div>
@@ -70,5 +70,5 @@ export default () => {
         </div>
       </div>
     </div>
-  `)
-}
+  `;
+};
